a9a20719bdb07501e03077fe879be3a8502af29f
r-toki <natto.and.eggs@gmail.com>
2023-11-12 14:37:03 +0900

    fix

--- functions/src/https/index.ts
+++ functions/src/https/index.ts
@@ -1,10 +1,16 @@
 import { onRequest } from "firebase-functions/v2/https";
 import { setGlobalOptions } from "firebase-functions/v2/options";
 
+import { sleep } from "~/utils";
+
 setGlobalOptions({
   region: "asia-northeast1",
 });
 
-export const healthCheck = onRequest((_req, res) => {
-  res.send("Good evening from firebase!");
+export const healthCheck = onRequest(async (_req, res) => {
+  await sleep(1_000);
+
+  res.json({
+    message: "Good evening from firebase!",
+  });
 });
