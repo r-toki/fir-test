60da81da1e6c51a926072e1e51e8b83308005dd5
r-toki <natto.and.eggs@gmail.com>
2023-11-12 13:16:54 +0900

    fix

--- frontend/src/root.tsx
+++ frontend/src/root.tsx
@@ -2,13 +2,15 @@ import { Link, Outlet } from "react-router-dom";
 
 export function Root() {
   return (
-    <div>
-      <div className="space-x-2">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/me">Me</Link>
+    <div className="container mx-auto py-4 prose max-w-screen-lg">
+      <div className="space-y-4">
+        <div className="space-x-2">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/me">Me</Link>
+        </div>
+        <Outlet />
       </div>
-      <Outlet />
     </div>
   );
 }
