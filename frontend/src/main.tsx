3c95b1b9a9fb0e31f07f1d68e6dfde1c1d40bfd3
r-toki <natto.and.eggs@gmail.com>
2023-11-12 13:02:15 +0900

    fix

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,7 @@ import "./firebase/config.ts";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { App } from "./_app.tsx";
+import { App } from "./app.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
