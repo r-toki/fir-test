88dcf6362794f8c47c90a5db19b18e3af8cf051b
r-toki <natto.and.eggs@gmail.com>
2023-11-16 14:25:25 +0900

    fix

--- e2e/cypress/e2e/browsing.cy.js
+++ e2e/cypress/e2e/browsing.cy.js
@@ -1,11 +1,11 @@
 it("Browsing some pages", () => {
   cy.visitHome();
   cy.waitLoading();
-  cy.screenshot("home");
+  cy.get("h1").contains("Home").should("be.visible");
 
   cy.visitAbout();
-  cy.screenshot("about");
+  cy.get("h1").contains("About").should("be.visible");
 
   cy.visitMe();
-  cy.screenshot("me");
+  cy.get("h1").contains("Me").should("be.visible");
 });
